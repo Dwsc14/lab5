3d68d98b0c03ab0baa02e6b67e5f8f2050b3ddae
Nguyễn Đức <ducna1462@gmail.com>
2023-02-27 21:02:48 +0700

    Update

--- test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        String nName = " Nguyen   Anh  Duc";
-        nName = nName.trim();
-        nName = nName.replaceAll("\\s+", " ");
-        System.out.println(nName);
-    }
-}
